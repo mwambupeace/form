7c0d073d6056eebc75613b7253d7f343a39b4a6e
mwambupeace <peacemwambu@gmail.com>
2018-09-14 19:50:07 +0300

    first commit

---
+++ dict.py
@@ -0,0 +1,4 @@
+b=dict()
+for x in range (1,16):
+    b[x]=x**2
+print(b)
---
+++ yob.py
@@ -0,0 +1,12 @@
+from datetime import date
+def age():
+    yob =int(input("enter year of birth"))
+    today_date=date.today().year
+    age.today_date_yob
+    if age <18:
+      print("minor")
+    elif age >=18 and age <=36:
+      print("youth")
+    else:
+       print("elder")
+age()
